1e755295199c5b5f366ad49bb740a81a8940be58
Wallace Monsores <wallacemonsoress@gmail.com>
2026-06-01 06:44:49 -0300

    Add files via upload

---
+++ validade-pwa/src/service/db.js
@@ -0,0 +1,33 @@
+import { openDB } from 'idb'
+
+const DB_NAME = 'validade-db'
+const DB_VERSION = 1
+const STORE_NAME = 'produtos'
+
+export async function getDB() {
+  return openDB(DB_NAME, DB_VERSION, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, {
+          keyPath: 'id',
+          autoIncrement: true
+        })
+      }
+    }
+  })
+}
+
+export async function salvarProduto(produto) {
+  const db = await getDB()
+  return db.add(STORE_NAME, produto)
+}
+
+export async function listarProdutos() {
+  const db = await getDB()
+  return db.getAll(STORE_NAME)
+}
+
+export async function removerProduto(id) {
+  const db = await getDB()
+  return db.delete(STORE_NAME, id)
+}
